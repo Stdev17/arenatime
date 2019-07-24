d3261768b2baa8af69b4cc47260185b134216a15
Leta <stdev17@gmail.com>
2019-07-24 23:59:56 +0900

    Alert and Form

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {
   Form,
   Col,
-  Button
+  Button,
+  Alert
 } from 'react-bootstrap';
 import { SetParty } from './SetParty';
 import '../css/daum.css';
@@ -36,17 +37,62 @@ export class Register extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedFile: null
+      selectedFile: null,
+      loaded: false,
+      overload: false,
+      form: {
+        attackPower: 0,
+        defencePower: 0,
+        attackStar: "",
+        defenceStar: "",
+        result: "",
+        arena: ""
+      }
     };
-    this.onChangeHandler = this.onChangeHandler.bind(this);
+    this.fileHandler = this.fileHandler.bind(this);
+    this.inputHandler = this.inputHandler.bind(this);
   }
-  onChangeHandler(e) {
-    if (e.target.files[0].size > 1) {
-      console.log(e.target.files[0].size);
+  fileHandler(e) {
+    if (e.target.files[0] == null) {
+      this.setState({
+        selectedFile: null,
+        loaded: false,
+        overload: false
+      });
+      return;
+    }
+    if (e.target.files[0].size > 51200) {
+      this.setState({
+        selectedFile: null,
+        loaded: false,
+        overload: true
+      });
+      return;
     }
     this.setState({
-      selectedFile: e.target.files[0]
+      selectedFile: e.target.files[0],
+      loaded: true,
+      overload: false
     });
+  }
+  inputHandler(e) {
+    let eName = e.target.name;
+    let eVal = e.target.value;
+    this.setState({form: {...this.state.form, [eName]: eVal}});
+  }
+  alert() {
+    if (this.state.overload) {
+      return (
+        <div>
+        <br/>
+        <Alert variant="danger">
+          <p>
+            파일 제한 용량을 초과했습니다.
+          </p>
+        </Alert>
+        </div>
+      );
+    }
   }
 
   render() {
@@ -63,30 +109,30 @@ export class Register extends React.Component {
           <Form.Row>
             <Form.Group as={Col} controlId="formGridCity">
               <Form.Label>공격덱 전투력</Form.Label>
-                <Form.Control />
+                <Form.Control name="attackPower" onChange={this.inputHandler}/>
             </Form.Group>
             <Form.Group as={Col} controlId="formGridCity">
               <Form.Label>방어덱 전투력</Form.Label>
-                <Form.Control />
+                <Form.Control name="defencePower" onChange={this.inputHandler}/>
             </Form.Group>
             <Form.Group as={Col} controlId="formGridCity">
               <Form.Label>공격덱 성급</Form.Label>
-                <Form.Control placeholder="(54434)"/>
+                <Form.Control placeholder="(54434)" name="attackStar" onChange={this.inputHandler}/>
             </Form.Group>
             <Form.Group as={Col} controlId="formGridCity">
               <Form.Label>방어덱 성급</Form.Label>
-                <Form.Control placeholder="(54434)"/>
+                <Form.Control placeholder="(54434)" name="defenceStar" onChange={this.inputHandler}/>
             </Form.Group>
             <Form.Group as={Col} controlId="formGridPosition">
               <Form.Label>결과</Form.Label>
-              <Form.Control as="select">
+              <Form.Control as="select" name="result" onChange={this.inputHandler}>
                 <option>방어덱 패배</option>
                 <option>방어덱 승리</option>
               </Form.Control>
             </Form.Group>
             <Form.Group as={Col} controlId="formGridPosition">
               <Form.Label>아레나</Form.Label>
-              <Form.Control as="select">
+              <Form.Control as="select" name="arena" onChange={this.inputHandler}>
                 <option>배틀 아레나</option>
                 <option>프린세스 아레나</option>
               </Form.Control>
@@ -112,7 +158,8 @@ export class Register extends React.Component {
       <p style={smallText} className="ten">
         {"500KB 이하의 .jpg .jpeg .png 파일을 올려주세요."}
         <br/>
-        <input type="file" name="file" accept=".jpg, .jpeg, .png" onChange={this.onChangeHandler}/>
+        <input type="file" name="file" accept=".jpg, .jpeg, .png" onChange={this.fileHandler}/>
+        {this.alert()}
       </p>
       <p style={subText} className="twenty">
         <Button variant='success'>

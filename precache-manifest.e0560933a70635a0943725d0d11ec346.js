0015cfa96201951b62812f1a35ebfcaa369eef95
Min Yoon <stdev17@gmail.com>
2019-08-01 21:34:20 +0900

    Updates

---
+++ precache-manifest.e0560933a70635a0943725d0d11ec346.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb436dfd03b45eeeec3650d2c26b327",
+    "url": "/arenatime/index.html"
+  },
+  {
+    "revision": "2e7246674b3deba3609a",
+    "url": "/arenatime/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "2eb0614317699a8880db",
+    "url": "/arenatime/static/css/main.589c50a6.chunk.css"
+  },
+  {
+    "revision": "2e7246674b3deba3609a",
+    "url": "/arenatime/static/js/2.611af341.chunk.js"
+  },
+  {
+    "revision": "2eb0614317699a8880db",
+    "url": "/arenatime/static/js/main.45d9347c.chunk.js"
+  },
+  {
+    "revision": "0803dd4244798aaf2acd",
+    "url": "/arenatime/static/js/runtime~main.791c5b6c.js"
+  }
+]);

5fe97bcd24b5a100003fa57cbdb5ebeccfdae416
Min Yoon <stdev17@gmail.com>
2019-08-02 21:52:36 +0900

    Updates

--- precache-manifest.40074258f35dcb5ebc87712b4c37d88c.js
+++ precache-manifest.40074258f35dcb5ebc87712b4c37d88c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "62a657c5d6a4355293b17ee14f724f16",
+    "revision": "24993aa6fb69294dc3a5e1d551be56e0",
     "url": "/arenatime/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/arenatime/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "c483fa841e31a680e733",
+    "revision": "44ad5bbd1eb718f5f9f5",
     "url": "/arenatime/static/css/main.923c89f2.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/arenatime/static/js/2.611af341.chunk.js"
   },
   {
-    "revision": "c483fa841e31a680e733",
-    "url": "/arenatime/static/js/main.e9d81d0a.chunk.js"
+    "revision": "44ad5bbd1eb718f5f9f5",
+    "url": "/arenatime/static/js/main.3171b6a6.chunk.js"
   },
   {
     "revision": "0803dd4244798aaf2acd",

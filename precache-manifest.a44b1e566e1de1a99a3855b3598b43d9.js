d757351c1576c9b8c4cee6a97a07c9fdb514ef5d
Min Yoon <stdev17@gmail.com>
2019-08-24 04:27:15 +0900

    Updates

--- precache-manifest.a44b1e566e1de1a99a3855b3598b43d9.js
+++ precache-manifest.a44b1e566e1de1a99a3855b3598b43d9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e7c24b471c909c60610da5a2d28f0831",
+    "revision": "3f9c09733593a0b262341a604712e526",
     "url": "/arenatime/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/arenatime/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "158ac563dac20f11e448",
+    "revision": "159a019639298e1f3d79",
     "url": "/arenatime/static/css/main.0d30028e.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/arenatime/static/js/2.10ffcd6c.chunk.js"
   },
   {
-    "revision": "158ac563dac20f11e448",
-    "url": "/arenatime/static/js/main.89f45ac4.chunk.js"
+    "revision": "159a019639298e1f3d79",
+    "url": "/arenatime/static/js/main.3b0759ba.chunk.js"
   },
   {
     "revision": "0803dd4244798aaf2acd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/arenatime/precache-manifest.fc234bec8439c9470b65d5dab6b98759.js"
+  "/arenatime/precache-manifest.a44b1e566e1de1a99a3855b3598b43d9.js"
 );
 
 self.addEventListener('message', (event) => {
